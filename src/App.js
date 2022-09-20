e5584bc86dfe8a0a0a470c6d1fb3c84bff636714
diegogaldino-dev <68260569+diegogaldino-dev@users.noreply.github.com>
2022-09-20 20:39:11 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,146 @@
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs";
+
+const API = "http://localhost:5000";
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  //load todos on page load
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const res = await fetch(API + "/Todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false);
+
+      setTodos(res);
+    };
+
+    loadData();
+
+    
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    setTodos((prevStats) => [...prevStats, todo]);
+
+    //zerando os inputs
+    setTitle("");
+    setTime("");
+  };
+
+  const handleDelete = async(id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevStats) => prevStats.filter((todo) => todo.id !== id))
+  }
+
+  const handleEdit = async(todo) => {
+    
+    todo.done = !todo.done
+
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    setTodos((prevStats) => 
+    prevStats.map((t) => (t.id === data.id) ? (t = data) : t))
+
+  }
+
+
+  if(loading) {
+    return <p>Aguarde um momento....</p>
+  }
+
+  return (
+    <div className="App">
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      <div className="form-todo">
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Titulo da tarefa"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input
+              type="text"
+              name="time"
+              placeholder="Tempo estimado(em horas)"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+          <input type="submit" value="Criar Tarefa" />
+        </form>
+      </div>
+
+      <div className="list-todo">
+        <h2>Listas de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração: {todo.time}</p>
+            <div className="actions">
+              <span onClick={() => handleEdit(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill/>}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
